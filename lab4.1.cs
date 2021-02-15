eeb0d478310dc292dfb2323d58ccef433a1c8c35
Viola8905 <74563100+Viola8905@users.noreply.github.com>
2021-02-15 18:20:52 +0200

    Add files via upload

---
+++ lab4.1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab4._1_сейшарп
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //лабораторна 4 завд1
+            Console.WriteLine("a=");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int p = 4 * a;
+            int s = a * a;
+            Console.WriteLine("s={0},p={1}", s, p);
+
+        }
+    }
+}
